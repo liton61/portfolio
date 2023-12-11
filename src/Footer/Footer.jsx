429638450fa433d21160734fe3316d51a0eadc39
liton61 <litonofficial61@gmail.com>
2023-12-12 00:44:18 +0600

    footer updated

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="footer footer-center p-4 bg-gray-900 border-t border-green-400 text-white p-10">
+            <footer className="footer footer-center bg-gray-900 border-t border-green-400 text-white p-10">
                 <aside>
                     <p className="font-medium">Copyright © 2023 - All right reserved by Liton</p>
                 </aside>
